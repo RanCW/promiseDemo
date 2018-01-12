95dba4af1e230f00072700bdf07fe728cbae97f7
ranchengwei <rancwdream@sina.com>
2018-01-12 14:37:32 +0800

    add all and race methods

--- es6MyPromise.js
+++ es6MyPromise.js
@@ -1,6 +1,11 @@
 /**
  * Created by pc100 on 2018/1/8 0008.
  */
+class promiseParent{
+
+}
+
+
 class MyPromise{
   constructor(callback){
     try{
@@ -15,6 +20,9 @@ class MyPromise{
     this.reason=undefined;
   }
   resolve(res){
+    if(res instanceof Promise){
+      return res.then(resolve,reject);
+    }
     if(this.status==='pending'){
       this.status='fulfilled';
       this.value=res;
@@ -32,6 +40,37 @@ class MyPromise{
       })
     }
   }
+  resolvePromise(promise2,x,resolve,reject){
+    let then;
+    //如果x就是promise2
+    if(promise2 === x){
+      return reject(new TypeError('循环引用'));
+    }
+    if(x instanceof Promise){
+      if(x.status == 'pending'){
+        x.then(function(y){
+          this.resolvePromise(promise2,y,resolve,reject);
+        },reject);
+      }else if(x.status == 'fulfilled'){
+        resolve(x.value);
+      }else if(x.status == 'rejected'){
+        reject(x.reason);
+      }
+    }else if(x!=null && (typeof x == 'object' || typeof x == 'function')){
+      try{
+        then = x.then;
+        if(typeof then == 'function'){
+          then.call(x,function(y){
+            this.resolvePromise(promise2,y,resolve,reject)
+          },reject);
+        }
+      }catch(e){
+        reject(e);
+      };
+    }else{
+      resolve(x);
+    }
+  }
   then(resolve,reject){
     resolve = typeof resolve == 'function'?resolve:function(value){return value};
     reject = typeof reject=='function'?reject:function(reason){
@@ -40,33 +79,85 @@ class MyPromise{
     this.resolve=resolve;
     this.reject=reject;
     let returnPromise;//返回的新promise
+    let self=this;
     if(this.status == 'pending'){//等待状态
-      let self=this;
-      returnPromise =new MyPromise(function (resolve, reject) {
-        self.onFulfilledCallback.push(resolve);
-        self.onRejectedCallback.push(reject);
-      })
-      // this.onFulfilledCallback.push(resolve)
-      // this.onRejectedCallback.push(reject)
+      returnPromise = new Promise(function(returnResolve,returnReject){
+        self.onFulfilledCallback.push(function(){
+          let x = resolve(self.value);
+          self.resolvePromise(returnPromise,x,returnResolve,returnReject);
+        });
+        self.onRejectedCallback.push(function(){
+          let x = reject(self.reason);
+          self.resolvePromise(returnPromise,x,returnResolve,returnReject);
+        });
+      });
     }
     if(this.status=='fulfilled'){//成功
-      this.resolve(this.value);
-
-      returnPromise=new MyPromise(function (resolve,reject) {})
+      returnPromise = new Promise(function(returnResolve,returnReject){
+        let x = resolve(self.value);
+        self.resolvePromise(returnPromise,x,returnResolve,returnReject);
+      });
     }
     if(this.status=='rejected'){//失败
-      this.reject(this.reason);
+      returnPromise = new Promise(function(returnResolve,returnReject){
+        let x = reject(self.reason);
+        self.resolvePromise(returnPromise,x,returnResolve,returnReject);
+      });
     }
+    return returnPromise;
+  }
+  catch(catchFunc){
+    this.then(null,catchFunc);
+  }
+  static all(arr){
+    return new MyPromise((resolve,reject)=>{
+      let result=[];
+      let resolveIndex=0;
+      let resolved=function (index) {
+        return function (data) {
+          result[index]=data;
+          resolveIndex++;
+          if(resolveIndex===arr.length){
+            resolve(result)
+          }
+        }
+      };
+      arr.forEach((item,index)=>{
+        item.then(resolved(index),err=>{
+          reject(err)
+        })
+      });
+    })
+  }
+  static race(arr){
+    return new MyPromise((resolve,reject)=>{
+      arr.forEach((item,index)=>{
+        item.then(res=>{
+          resolve(res);
+        },err=>{
+          reject(err)
+        })
+      });
+    })
   }
 }
 
-let test=new MyPromise(function (resolve, reject) {
+let test1=new MyPromise(function (resolve, reject) {
   setTimeout(function () {
-    resolve(1000)
-  },2000)
-})
-test.then(function (res) {
-  console.log(res+'成功')
-},function (err) {
-  console.log(err+'错误')
+    reject(1000)
+  },400)
+});
+
+let test2=new MyPromise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve(2000)
+  },100)
+});
+
+let all=MyPromise.race([test1,test2])
+
+all.then(res=>{
+  console.log(res)
+},err=>{
+  console.log(err+'err');
 })
--- origin.js
+++ origin.js
@@ -7,8 +7,27 @@ let  test=new Promise(function (resolve, reject) {
   },2000)
 })
 
-test.then(res=>{
-  console.log(res)
+// test.then(res=>{
+//   console.log(res)
+// }).catch(err=>{
+//   console.log(err);
+// })
+
+var winnerPromise = new Promise(function (resolve,reject) {
+  setTimeout(function () {
+    resolve('this is winner');
+  }, 800);
+});
+var loserPromise = new Promise(function (resolve,reject) {
+  setTimeout(function () {
+    reject('this is loser');
+  }, 1000);
+});
+// 第一个promise变为resolve后程序停止
+Promise.race([winnerPromise, loserPromise]).then(function (value) {
+  console.log(value);    // => 'this is winner'
+}).then(res=>{
+  console.log(res);
 }).catch(err=>{
   console.log(err);
-})
+});
